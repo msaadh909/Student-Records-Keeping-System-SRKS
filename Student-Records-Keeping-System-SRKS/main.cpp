03dc0e8d2d267fe5d857504fd7665c9e401679b9
msaadh909 <msaadh909@gmail.com>
2022-05-11 06:49:24 +0500

    Initial Commit

---
+++ Student-Records-Keeping-System-SRKS/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Student-Records-Keeping-System-SRKS
+//
+//  Created by Saad Hassan on 5/11/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
